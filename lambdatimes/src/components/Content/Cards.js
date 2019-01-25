8b314bdb500b87e085482d07bb2575df6d01e1e6
samjeffcoat <samjeffcoat@gmail.com>
2019-01-25 11:53:24 -0600

    Adds mapping for the Cards prop and creating a new card component passing card as a prop

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -6,10 +6,13 @@ const Cards = props => {
     <div className="cards-container">
       {/* Using the cards prop, map over the list creating a 
           new Card component for each passing the card as the only prop*/}
+
+          {props.cards.map((c, i)=> <Card key={i} card={c}/>)}
     </div>
   )
 }
 
 // Make sure you include prop types for all of your incoming props
+
 
 export default Cards;
