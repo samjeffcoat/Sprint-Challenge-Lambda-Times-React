9d1c1b08be7cd8ab8aa80b03ba02ab41e922b0ae
samjeffcoat <samjeffcoat@gmail.com>
2019-01-25 11:01:17 -0600

    Adds 2 props to the tabs components. SelectedTab-Currently Se;ected and selectTabHandler that indcluded the function to change the selected Tab

--- lambdatimes/src/components/Content/Content.js
+++ lambdatimes/src/components/Content/Content.js
@@ -55,8 +55,9 @@ export default class Content extends Component {
           Add 2 props to the Tabs component, 
           `selectedTab` that includes the currently selected tab
           and `selectTabHandler` that includes the function to change the selected tab
-        */}
-        <Tabs tabs={this.state.tabs} />
+        */
+       }
+        <Tabs tabs={this.state.tabs} selectedTab= {this.state.selected} selectTabHandler= {this.changeSelected}/>
         <Cards cards={this.filterCards()} />
       </div>
     );
