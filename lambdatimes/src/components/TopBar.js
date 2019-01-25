596bf287c61844ebc1d62a023997d37c7b1ba599
samjeffcoat <samjeffcoat@gmail.com>
2019-01-25 12:41:09 -0600

    Adds styling for ContainerLeft

--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -25,25 +25,35 @@ const TopBarContainer= styled.div`
   padding: 0 10px;
 @media(min-width: 1280px){
   width:1280px;
-}
+}`;
 
-
-
+const ContainerLeft = styled.div`
+display: flex;
+  justify-content: none;
+  align-items: center;
+  flex-direction: row;
+  flex: 1;
+  font-size: 11px;
+  span{
+    cursor: pointer;
+  margin-right: 25%;
+  font-weight: bold;
+  }
 `;
 
 const TopBar = () => {
   return (
     <TopBarFull>
       <TopBarContainer>
-        <div className="container-left">
+        <ContainerLeft>
           <span>TOPICS</span><span>SEARCH</span>
-        </div>
+        </ContainerLeft>
         <div className="container-center">
           <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
         </div>
         <div className="container-right">
           <span>LOG IN</span>
-        </div>
+        </ContainerLeft>
         </TopBarContainer>
       </TopBarFull>
   )
