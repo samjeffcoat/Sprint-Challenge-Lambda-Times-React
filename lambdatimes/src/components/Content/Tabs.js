260e9a3a626718be059bd5eaeb0ade2160b30f6a
samjeffcoat <samjeffcoat@gmail.com>
2019-01-25 11:16:57 -0600

    Adds the map function so each tab selected creates a new tab for each one. Gives the Tab the props of Selected Tab and selectedTabHandler

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -7,6 +7,8 @@ const Tabs = props => {
         <span className="title">TRENDING TOPICS:</span>
         {/* map over the tabs provided on your props, create a new Tab component for each one.
             give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
+
+            {props.tabs.map((t, i)=> <Tab key= {i} tab={t}selectedTab={props.selectedTab} selectedTabHandler= {props.selectedTabHandler} />)}
       </div>
     </div>
   );
