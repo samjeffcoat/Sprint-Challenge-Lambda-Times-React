9447d638e5a9cef482caac3b02b212b446a5147d
samjeffcoat <samjeffcoat@gmail.com>
2019-01-25 11:56:52 -0600

    Adds prop checking for Cards component

--- lambdatimes/src/components/Content/Cards.js
+++ lambdatimes/src/components/Content/Cards.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Card from './Card';
 
+import PropTypes from 'prop-types';
+
 const Cards = props => {
   return (
     <div className="cards-container">
@@ -13,6 +15,8 @@ const Cards = props => {
 }
 
 // Make sure you include prop types for all of your incoming props
-
+Cards.propTypes = {
+  cards: PropTypes.array.isRequired
+}
 
 export default Cards;
