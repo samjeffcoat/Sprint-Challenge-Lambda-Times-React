c70f488afb0e0b301b5ee515a90eaaaa3ac7ad1e
samjeffcoat <samjeffcoat@gmail.com>
2019-01-25 12:03:38 -0600

    adds author to card page

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -8,7 +8,7 @@ const Card = props => {
         <div className="img-container">
           <img src={props.card.img} />
         </div>
-        <span>By {/* author goes here */}</span>
+        <span>By {props.card.author}</span>
       </div>
     </div>
   );
